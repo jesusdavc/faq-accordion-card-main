c5475aa88378052f6f443808f4a7db870314f85b
Jesús Cuéllar <jedave.co@gmail.com>
2021-01-28 17:02:12 -0400

    Añadiendo interactividad con JavaScript

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const n = 2
-const element = document.getElementById(`element${n}`);
-/**Probar si se pueden llamar elementos así */
-const question = document.getElementsByClassName('element-question');
-const answer = document.getElementById(`answer${n}`);
+/*Call the elements */
+const element = document.getElementsByClassName('faq-answers_element');
 
 
-function showHiddeAnswer(){
-    if(answer.style.display != 'block'){
-        answer.style.display = 'block';
-}else{
-    answer.style.display = 'none';
+
+/**For each element that have the class apply a Event Listener */
+
+for(let i=0; i<=element.length; i++){
+    element[i].addEventListener('click', () => {
+        
+        const answer = document.getElementsByClassName('element-answer');
+        const question = document.getElementsByClassName('element-question');
+        const arrow_button = document.getElementsByClassName('arrow-button')
+        if (answer[i].style.display != 'block'){
+                answer[i].style.display = 'block';
+                question[i].style.fontWeight = 'bold';
+                arrow_button[i].style.transform = 'rotate(0.5turn)';
+        }else{
+            answer[i].style.display = 'none';
+            question[i].style.fontWeight = 'normal'
+            arrow_button[i].style.transform = 'rotate(0turn)';
+            }
+    
+        }
+    );
 }
-}
 
-element.addEventListener('click', showHiddeAnswer);
+
+
+
+/**  Funtion that determine what show and what hide*/
+
+
